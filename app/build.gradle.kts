67ed735cd46714a7c9466f8510ef99ef318451ca
sha19hank <shanks07p@gmail.com>
2026-06-13 13:44:57 +0530

    chore: bump versionCode to 6 for final Play Console upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.mlue.app"
         minSdk = 24
         targetSdk = 35
-        versionCode = 5
+        versionCode = 6
         versionName = "1.0.0"
 
         vectorDrawables {
